36df7e768c83c31d7e10339775646135896d945a
Anurag MK <anuragmk10@gmail.com>
2022-12-30 22:00:35 +0530

    first commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+function Header(){
+    return (
+        <div className='header d-flex align-items-center justify-content-space-between '>
+            <div className='header-left-items d-flex align-items-center maxy-30'> </div>
+       
+        </div>
+    )
+}
+
+export default Header
